8aaa584b1ed19774055eac6c760ee869de094a68
KurozawaChiaki <h1kari@connect.hku.hk>
2025-06-10 16:03:22 +0800

    feat: mvlm tool can generate landmarks for sculptor meshes

--- test_mvlm.py
+++ test_mvlm.py
@@ -2,10 +2,15 @@ from pathlib import Path
 import numpy as np
 import mvlm
 
-file = Path("testing/test2.obj")
+file = Path("testing/sculptor_neutral.obj")
 
-dm = mvlm.pipeline.create_pipeline("face_alignment")
+dm = mvlm.pipeline.create_pipeline(
+    "face_alignment",
+    render_image_stack=True,
+    render_image_folder="testing/mvlm/render_image_stack",
+)
 landmarks = dm.predict_one_file(file)
+np.save("testing/sculptor/landmarks.npy", landmarks)
 
 mvlm.utils.VTKViewer(file.as_posix(), landmarks)
 
